bb8737a7edee1d43bc1916cc93a4a2945b589baa
Alex John Suarez <alexjohnsuarez@gmail.com>
2020-11-17 18:09:24 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class DummyComponent extends Component {
+  render () {
+    return (
+      <div>I am a dummy react npm module</div>
+    )
+  }
+}
